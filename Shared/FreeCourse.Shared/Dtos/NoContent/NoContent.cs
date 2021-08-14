d0698752c14429c1212a5545a245cc0d00cef096
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-08-14 23:23:19 +0300

    Create Response DTOS

---
+++ Shared/FreeCourse.Shared/Dtos/NoContent/NoContent.cs
@@ -0,0 +1,6 @@
+﻿namespace FreeCourse.Shared.Dtos.NoContent
+{
+    public class NoContent
+    {
+    }
+}
---
+++ Shared/FreeCourse.Shared/Dtos/Response/ResponseDTO.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Text.Json.Serialization;
+
+namespace FreeCourse.Shared.Dtos.Response
+{
+    public class ResponseDTO<T>
+    {
+        public T Data { get; private set; }
+
+        [JsonIgnore]
+        public HttpStatusCode StatusCode { get; set; }
+
+        [JsonIgnore]
+        public bool IsSuccessful { get; private set; }
+
+        public List<string> Errors { get; set; }
+
+        public static ResponseDTO<T> Success(T data, HttpStatusCode statusCode)
+        {
+            return new ResponseDTO<T>
+            {
+                Data = data,
+                StatusCode = statusCode,
+                IsSuccessful = true
+            };
+        }
+
+        public static ResponseDTO<T> Success(HttpStatusCode statusCode)
+        {
+            return new ResponseDTO<T>
+            {
+                Data = default(T),
+                StatusCode = statusCode,
+                IsSuccessful = true
+            };
+        }
+
+        public static ResponseDTO<T> Fail(List<string> errors, HttpStatusCode statusCode)
+        {
+            return new ResponseDTO<T>
+            {
+                StatusCode = statusCode,
+                IsSuccessful = false,
+                Errors = errors
+            };
+        }
+
+        public static ResponseDTO<T> Fail(string error, HttpStatusCode statusCode)
+        {
+            return new ResponseDTO<T>
+            {
+                StatusCode = statusCode,
+                IsSuccessful = false,
+                Errors = new List<string> { error }
+            };
+        }
+    }
+}
