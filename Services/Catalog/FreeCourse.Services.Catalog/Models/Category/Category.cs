3a63d13014e61d81bf6280983198db1c540ad18e
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-08-14 23:01:14 +0300

    Create Models

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Category/Category.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Models.Category
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Feature/Feature.cs
@@ -0,0 +1,8 @@
+﻿namespace FreeCourse.Services.Catalog.Models.Feature
+{
+    public class Feature
+    {
+        public int Duration { get; set; }
+
+    }
+}
