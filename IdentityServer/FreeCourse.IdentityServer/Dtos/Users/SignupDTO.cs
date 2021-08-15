92af27c5ceae42bb8332ff77705d60cc2046b7f4
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-08-15 20:11:45 +0300

    Create User Controller For Identity

---
+++ IdentityServer/FreeCourse.IdentityServer/Dtos/Users/SignupDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace FreeCourse.IdentityServer.Dtos.Users
+{
+    public class SignupDTO
+    {
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string City { get; set; }
+
+    }
+}
