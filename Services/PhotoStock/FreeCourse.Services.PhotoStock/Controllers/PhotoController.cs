e221982e6a5aa9e869764579d9156b8fcdb6598b
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-08-17 22:08:05 +0300

    Add PhotoSave

---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotoController.cs
@@ -0,0 +1,37 @@
+﻿using FreeCourse.Services.PhotoStock.Dtos.photos;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Dtos.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if (photo != null && photo.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+                using var stream = new FileStream(path, FileMode.Create);
+                await photo.CopyToAsync(stream, cancellationToken);
+
+                var returnPath = $"photos/{photo.FileName}";
+
+                PhotoDTO photoDTO = new() { URL = returnPath };
+
+                return CreateActionResultInstance(ResponseDTO<PhotoDTO>.Success(photoDTO, HttpStatusCode.OK));
+            }
+
+            return CreateActionResultInstance(ResponseDTO<PhotoDTO>.Fail("photo is empty", HttpStatusCode.BadRequest));
+        }
+    }
+}
---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Dtos/photos/PhotoDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace FreeCourse.Services.PhotoStock.Dtos.photos
+{
+    public class PhotoDTO
+    {
+        public string URL { get; set; }
+    }
+}
