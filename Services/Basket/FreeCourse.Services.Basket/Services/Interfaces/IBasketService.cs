9ab66515982149a25b95bf1235e4fcb83baaa937
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-08-18 20:55:32 +0300

    Add IBasketService

---
+++ Services/Basket/FreeCourse.Services.Basket/Services/Interfaces/IBasketService.cs
@@ -0,0 +1,15 @@
+﻿using FreeCourse.Services.Basket.Dtos.Baskets;
+using FreeCourse.Shared.Dtos.Response;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Basket.Services.Interfaces
+{
+    public interface IBasketService
+    {
+        Task<ResponseDTO<BasketDTO>> GetBasket(string userId);
+
+        Task<ResponseDTO<bool>> SaveOrUpdate(BasketDTO basket);
+
+        Task<ResponseDTO<bool>> Delete(string userId);
+    }
+}
