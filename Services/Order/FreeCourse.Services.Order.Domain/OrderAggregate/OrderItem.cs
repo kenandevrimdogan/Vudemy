474181f8d6c95133ac2ef1f927adf5c7715b3a96
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-08-24 21:57:28 +0300

    Add IAggregateRoot, Entity classes

---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,30 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class OrderItem : Entity
+    {
+        public OrderItem(string productId, string productName, string pictureURL, decimal price)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            PictureURL = pictureURL;
+            Price = price;
+        }
+
+        public string ProductId { get; private set; }
+
+        public string ProductName { get; private set; }
+
+        public string PictureURL { get; private set; }
+
+        public decimal Price { get; private set; }
+
+        public void UpdateOrderItem(string productName, string pictureURL, decimal price)
+        {
+            ProductName = productName;
+            PictureURL = pictureURL;
+            Price = price;
+        }
+    }
+}
