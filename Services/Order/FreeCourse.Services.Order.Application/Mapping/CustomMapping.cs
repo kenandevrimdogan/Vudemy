be6be987ea796b5edb41fabd88dc76ceda3aaf41
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-08-24 22:31:19 +0300

    Add Custom and Mapper

---
+++ Services/Order/FreeCourse.Services.Order.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Order.Application.Dtos.Orders;
+
+namespace FreeCourse.Services.Order.Application.Mapping
+{
+    public class CustomMapping: Profile
+    {
+        public CustomMapping()
+        {
+            CreateMap<Domain.OrderAggregate.Order, OrderDTO>().ReverseMap();
+            CreateMap<Domain.OrderAggregate.OrderItem, OrderItemDTO>().ReverseMap();
+            CreateMap<Domain.OrderAggregate.Address, AddressDTO>().ReverseMap();
+        }
+    }
+}
