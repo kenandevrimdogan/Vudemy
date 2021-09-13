77bba64f49e77dcc5dd7614e1aa21aec31fa0e72
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-09-13 21:49:54 +0300

    Created RecivePayment in Fake Payments Controller

---
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -0,0 +1,19 @@
+﻿using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Dtos.NoContent;
+using FreeCourse.Shared.Dtos.Response;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace FreeCourse.Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentsController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult RecivePayment()
+        {
+            return CreateActionResultInstance<NoContent>(ResponseDTO<NoContent>.Success(HttpStatusCode.OK)); 
+        }
+    }
+}
