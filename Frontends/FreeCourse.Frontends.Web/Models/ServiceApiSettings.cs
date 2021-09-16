48a9f4e2576d7b35d0b9f5a9aaf9c039dc5a9f98
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-09-16 21:10:08 +0300

    Added ServiceApiSettings

---
+++ Frontends/FreeCourse.Frontends.Web/Models/ServiceApiSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace FreeCourse.Frontends.Web.Models
+{
+    public class ServiceApiSettings
+    {
+        public string BaseUri { get; set; }
+
+        public string PhotoStockUri { get; set; }
+
+    }
+}
