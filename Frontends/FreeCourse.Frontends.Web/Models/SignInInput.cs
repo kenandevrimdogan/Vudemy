b992d2eed9cbb63f31493bd968f23e896993d6e1
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-09-16 21:03:16 +0300

    Added Idendity Interface

---
+++ Frontends/FreeCourse.Frontends.Web/Models/SignInInput.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Frontends.Web.Models
+{
+    public class SignInInput
+    {
+        [Display(Name = "Email Adresiniz")]
+        public string Email { get; set; }
+
+        [Display(Name = "Şifreniz")]
+        public string Password { get; set; }
+
+        [Display(Name = "Beni Hatırla")]
+        public bool IsRemember { get; set; }
+    }
+}
---
+++ Frontends/FreeCourse.Frontends.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,17 @@
+﻿using FreeCourse.Frontends.Web.Models;
+using FreeCourse.Shared.Dtos.Response;
+using IdentityModel.Client;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Frontends.Web.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<ResponseDTO<bool>> SignIn(SignInInput signInInput);
+
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+
+        Task RevokeRefreshToken();
+
+    }
+}
