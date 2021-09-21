e13ef276dc8f28bf3ba4af4a4c7c41612ea00fa4
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-09-21 22:44:58 +0300

    Update GetUser service method

--- Frontends/FreeCourse.Frontends.Web/Services/Abstracts/UserService.cs
+++ Frontends/FreeCourse.Frontends.Web/Services/Abstracts/UserService.cs
@@ -1,18 +1,23 @@
 ﻿using FreeCourse.Frontends.Web.Models;
 using FreeCourse.Frontends.Web.Services.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace FreeCourse.Frontends.Web.Services.Abstracts
 {
     public class UserService : IUserService
     {
+        private readonly HttpClient _httpClient;
 
-        public Task<UserViewModel> GetUser()
+        public UserService(HttpClient httpClient)
         {
-            throw new NotImplementedException();
+            _httpClient = httpClient;
+        }
+
+        public async Task<UserViewModel> GetUser()
+        {
+            return await _httpClient.GetFromJsonAsync<UserViewModel>("/api/user/getuser");
         }
     }
 }
