49701f2fa06cb52bb538fa6bf4a676945275513f
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-10-21 23:01:30 +0300

    Add Basket Service

---
+++ Frontends/FreeCourse.Frontends.Web/Services/Abstracts/BasketService.cs
@@ -0,0 +1,44 @@
+﻿using FreeCourse.Frontends.Web.Models.Baskets;
+using FreeCourse.Frontends.Web.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Frontends.Web.Services.Abstracts
+{
+    public class BasketService : IBasketService
+    {
+        public Task AddBasketItem(BasketItemViewModel basketItem)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> ApplyDiscount(string discountCode)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> CancelAplyDiscount()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> Delete()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<BasketViewModel> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> RemoveBasketItem(string courseId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> SaveOrUpdate(BasketViewModel basketViewModel)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Frontends/FreeCourse.Frontends.Web/Services/Interfaces/IBasketService.cs
@@ -0,0 +1,22 @@
+﻿using FreeCourse.Frontends.Web.Models.Baskets;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Frontends.Web.Services.Interfaces
+{
+    public interface IBasketService
+    {
+        Task<bool> SaveOrUpdate(BasketViewModel basketViewModel);
+
+        Task<BasketViewModel> Get();
+
+        Task<bool> Delete();
+
+        Task AddBasketItem(BasketItemViewModel basketItem);
+
+        Task<bool> RemoveBasketItem(string courseId);
+
+        Task<bool> ApplyDiscount(string discountCode);
+
+        Task<bool> CancelAplyDiscount();
+    }
+}
