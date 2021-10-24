0703271a07507c080a045ec1b75cc28bd5602329
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-10-24 19:27:15 +0300

    Add Baskets Controller

---
+++ Frontends/FreeCourse.Frontends.Web/Controllers/BasketsController.cs
@@ -0,0 +1,49 @@
+﻿using FreeCourse.Frontends.Web.Models.Baskets;
+using FreeCourse.Frontends.Web.Services.Interfaces;
+using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Frontends.Web.Controllers
+{
+    public class BasketsController : Controller
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+
+        public BasketsController(ICatalogService catalogService, IBasketService basketService)
+        {
+            _catalogService = catalogService;
+            _basketService = basketService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var basket = await _basketService.Get();
+            return View(basket);
+        }
+
+        public async Task<IActionResult> AddBasketItem(string courseId)
+        {
+            var course = await _catalogService.GetByCourseIdAsync(courseId);
+
+            var basketItem = new BasketItemViewModel
+            {
+                CourseId = course.Id,
+                CourseName = course.Name,
+                Price = course.Price,
+            };
+
+            await _basketService.AddBasketItem(basketItem);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> RemoveBasketItem(string courseId)
+        {
+            await _basketService.RemoveBasketItem(courseId);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
