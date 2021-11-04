4c4eeba3b9f55af50511c39a2ea9330d1b8501db
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-11-04 23:53:39 +0300

    Update Orders

--- Frontends/FreeCourse.Frontends.Web/Controllers/OrdersController.cs
+++ Frontends/FreeCourse.Frontends.Web/Controllers/OrdersController.cs
@@ -23,5 +23,27 @@ namespace FreeCourse.Frontends.Web.Controllers
 
             return View(new CheckoutInfoInput());
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Checkout(CheckoutInfoInput checkoutInfoInput)
+        {
+            var orderStatus = await _orderservice.CreateOrder(checkoutInfoInput);
+
+            if (!orderStatus.IsSuccess)
+            {
+                var basket = await _basketService.Get();
+                ViewBag.basket = basket;
+                ViewBag.error = orderStatus.ErrorMessage;
+                return View();
+            }
+
+            return RedirectToAction(nameof(SuccessfullCheckout), new { orderId = orderStatus.OrderId });
+        }
+
+        public IActionResult SuccessfullCheckout(int orderId)
+        {
+            ViewBag.orderId = orderId;
+            return View();
+        } 
     }
 }
