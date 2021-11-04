1e2cfee369c6c5e52d3637bb0f5fc5bc30476683
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-11-04 23:45:18 +0300

    Add Checkout page

---
+++ Frontends/FreeCourse.Frontends.Web/Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+﻿using FreeCourse.Frontends.Web.Models.Orders;
+using FreeCourse.Frontends.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Frontends.Web.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IBasketService _basketService;
+        private readonly IOrderService _orderservice;
+
+        public OrdersController(IBasketService basketService, IOrderService orderservice)
+        {
+            _basketService = basketService;
+            _orderservice = orderservice;
+        }
+
+        public async Task<IActionResult> Checkout()
+        {
+            var basket = await _basketService.Get();
+            ViewBag.basket = basket;
+
+            return View(new CheckoutInfoInput());
+        }
+    }
+}
