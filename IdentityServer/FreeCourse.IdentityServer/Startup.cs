3729973d7ba7532b369797ea1e40ac73f9429410
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-11-06 23:47:09 +0300

    Add TokenExchangeExtensionGranValidator

--- IdentityServer/FreeCourse.IdentityServer/Startup.cs
+++ IdentityServer/FreeCourse.IdentityServer/Startup.cs
@@ -57,6 +57,7 @@ namespace FreeCourse.IdentityServer
                 .AddAspNetIdentity<ApplicationUser>();
 
             builder.AddResourceOwnerValidator<IdentityResourceOwnerPasswordValidator>();
+            builder.AddExtensionGrantValidator<TokenExchangeExtensionGranValidator>();
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
