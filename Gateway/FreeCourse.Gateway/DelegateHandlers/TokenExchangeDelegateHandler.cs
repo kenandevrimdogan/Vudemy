752677f078438fd3c68f6a1dc2bcda5d53b995c5
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-11-07 00:12:48 +0300

    Add TokenExchangeDelegateHandler

---
+++ Gateway/FreeCourse.Gateway/DelegateHandlers/TokenExchangeDelegateHandler.cs
@@ -0,0 +1,73 @@
+﻿using IdentityModel.Client;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Gateway.DelegateHandlers
+{
+    public class TokenExchangeDelegateHandler : DelegatingHandler
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+        private string _accessToken;
+
+        public TokenExchangeDelegateHandler(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+
+        private async Task<string> GetToken(string requesttoken)
+        {
+            if (!string.IsNullOrEmpty(_accessToken))
+            {
+                return _accessToken;
+            }
+
+            var disko = await _httpClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+            {
+                Address = _configuration["IdentityServerURL"],
+                Policy = new DiscoveryPolicy { RequireHttps = false }
+            });
+
+            if (disko.IsError)
+            {
+                throw disko.Exception;
+            }
+
+            var tokenExchangeTokenRequest = new TokenExchangeTokenRequest()
+            {
+                Address = disko.TokenEndpoint,
+                ClientId = _configuration["ClientId"],
+                ClientSecret = _configuration["ClientSecret"],
+                GrantType = _configuration["TokenGrantType"],
+                SubjectToken = requesttoken,
+                SubjectTokenType = _configuration["SubjectTokenType"],
+                Scope = "openid discount_fullpermission payment_fullpermission"
+            };
+
+            var tokenResponse = await _httpClient.RequestTokenExchangeTokenAsync(tokenExchangeTokenRequest);
+
+            if (tokenResponse.IsError)
+            {
+                throw tokenResponse.Exception;
+            }
+
+            _accessToken = tokenResponse.AccessToken;
+
+            return _accessToken;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var requestToken = request.Headers.Authorization.Parameter;
+
+            var newToken = await GetToken(requestToken);
+
+            request.SetBearerToken(newToken);
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
